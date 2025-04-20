01dc47b4687251c5f87263439dbb00f433466df7
Sam Smith <ssmith27@wooster.edu>
2025-04-19 22:24:50 -0400

    notes now write to file

--- src/initialize_txt_file.zig
+++ src/initialize_txt_file.zig
@@ -2,7 +2,9 @@ const std = @import("std");
 
 pub fn notes_txt_init() !std.fs.File {
     const cwd = std.fs.cwd();
-    const file = try cwd.openFile("notes.txt", .{ .read_write = true }) catch |err| {
+    const file = try cwd.openFile("notes.txt", .{
+        .read = true,
+    }) catch |err| {
         if (err == error.FileNotFound) {
             return cwd.createFile("notes.txt", .{ .read = true });
         } else {

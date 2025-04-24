6680cbed14d5d064e66200683830506acbabf95d
Sam Smith <ssmith27@wooster.edu>
2025-04-23 23:00:16 -0400

    added color to banner

--- src/banner.zig
+++ src/banner.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const stdout = std.io.getStdOut().writer();
 
 pub fn run_banner() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
@@ -6,13 +7,18 @@ pub fn run_banner() !void {
 
     const result = try std.process.Child.run(.{
         .allocator = allocator,
-        .argv = &[_][]const u8{ "figlet", "-f", "big", "NoteZ" },
+        .argv = &[_][]const u8{ "figlet", "-f", "ntgreek", "NoteZ" }, // big, ntgreek, speed, starwars
     });
 
     defer allocator.free(result.stdout);
 
-    try std.io.getStdOut().writeAll(result.stdout);
+    const ansi_blue = "\x1B[34m";
+    const ansi_reset_blue = "\x1B[0m";
+    const ansi_deep_orange = "\x1B[38;5;208m";
+    const ansi_reset_orange = "\x1B[0m";
+    try stdout.print("{s}{s}{s}", .{ansi_deep_orange, result.stdout, ansi_reset_orange});
 
-    try std.io.getStdOut().writer().writeAll("CMDS: * (add) | -done | -del | -clear | -show | -p \n");
+    const commands = "CMDS: * (add) | -done | -del | -clear | -show | -p \n";
+    try std.io.getStdOut().writer().print("{s}{s}{s}", .{ansi_blue, commands, ansi_reset_blue});
     std.debug.print("\n", .{});
 }
